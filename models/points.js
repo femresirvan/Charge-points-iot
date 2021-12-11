f63db00048be367bcc82a65cf8f806db28da1e82
femresirvan <femresirvan@gmail.com>
2021-12-11 03:43:21 +0300

    mongoschema comp.

---
+++ models/points.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const pointSchema = new mongoose.Schema({
+    locationx: {
+        type: Number,
+    },
+    locationy: {
+        type: Number
+    },
+    type: String,
+    slots: [{
+        slotName: {
+            type: String
+        },
+        isFull: {
+            type: Boolean,
+            default: false
+        }
+    }]
+});
+
+//To make 2 fields unique together
+pointSchema.index({ locationx: 1, locationy: 1 }, { unique: true })
+
+const Point = mongoose.model('Point', pointSchema);
+
+module.exports = Point;
